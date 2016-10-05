89321eed8b014b9a4bc746c3d96ac74a2f9adb23
ijamardo <ijamardo@ijamardo-glaptop.roam.corp.google.com>
2016-10-05 19:21:05 +0200

    The VRPickingPointAndPlane structure was missing from the backup.

---
+++ third_party/WebKit/Source/modules/vr/VRPickingPointAndPlane.cpp
@@ -0,0 +1,37 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/vr/VRPickingPointAndPlane.h"
+
+namespace blink {
+
+VRPickingPointAndPlane::VRPickingPointAndPlane(): m_correctValues(false)
+{
+    m_point = DOMFloat32Array::create(3);
+    m_plane = DOMFloat32Array::create(4);
+}
+
+void VRPickingPointAndPlane::setPickingPointAndPlane(const device::blink::VRPickingPointAndPlanePtr& pickingPointAndPlanePtr)
+{
+    m_correctValues = false;
+
+    if (pickingPointAndPlanePtr.is_null())
+        return;
+
+    for (size_t i = 0; i < 3; i++) {
+    	m_point->data()[i] = (float)pickingPointAndPlanePtr->point[i];
+    }
+    for (size_t i = 0; i < 4; i++) {
+    	m_plane->data()[i] = (float)pickingPointAndPlanePtr->plane[i];
+    }
+    m_correctValues = true;
+}
+
+DEFINE_TRACE(VRPickingPointAndPlane)
+{
+    visitor->trace(m_point);
+    visitor->trace(m_plane);
+}
+
+} // namespace blink
---
+++ third_party/WebKit/Source/modules/vr/VRPickingPointAndPlane.h
@@ -0,0 +1,37 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef VRPickingPointAndPlane_h
+#define VRPickingPointAndPlane_h
+
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "core/dom/DOMTypedArray.h"
+#include "device/vr/vr_service.mojom-blink.h"
+#include "platform/heap/Handle.h"
+#include "wtf/Forward.h"
+
+namespace blink {
+
+class VRPickingPointAndPlane final : public GarbageCollected<VRPickingPointAndPlane>, public ScriptWrappable {
+    DEFINE_WRAPPERTYPEINFO();
+public:
+    VRPickingPointAndPlane();
+    
+    DOMFloat32Array* point() const { return m_point; }
+    DOMFloat32Array* plane() const { return m_plane; }
+    bool correctValues() const { return m_correctValues; }
+
+    void setPickingPointAndPlane(const device::blink::VRPickingPointAndPlanePtr&);
+
+    DECLARE_VIRTUAL_TRACE();
+
+private:
+    Member<DOMFloat32Array> m_point;
+    Member<DOMFloat32Array> m_plane;
+    bool m_correctValues;
+};
+
+} // namespace blink
+
+#endif // VRPickingPointAndPlane_h
